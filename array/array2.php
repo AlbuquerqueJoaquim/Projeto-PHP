3749039987217e3f82d1f1351f30663b3a709e54
AlbuquerqueJR <joaquimalbuquerque.ti@gmail.com>
2018-07-17 21:51:20 -0400

    Arrays em PHP

---
+++ array/array2.php
@@ -0,0 +1,30 @@
+<?php
+
+    $carros[0][0] = "FIAT";
+    $carros[0][1] = "Palio";
+    $carros[0][2] = "UNO";
+
+    $carros[1][0] = "NISSAN";
+    $carros[1][1] = "March";
+    $carros[1][2] = "Versa";
+
+    echo $carros[0][2];
+    echo "<br>";
+    echo end($carros[1]);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Array Vetor   </title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.27" />
+</head>
+
+<body>
+
+</body>
+
+</html>
