6328435d9f3b3a14d24abc4a2fe0a18dc4674b4a
AlbuquerqueJR <joaquimalbuquerque.ti@gmail.com>
2018-07-22 20:49:13 -0400

    Trabalhando Com session em PHP

---
+++ session/config.php
@@ -0,0 +1,24 @@
+<?php
+
+    require_once("config.php");
+
+      //apagando as variasveis de sessao
+    session_unset($_SESSION['nome']);
+    echo $_SESSION['nome'];
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Config PHP   </title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.27" />
+</head>
+
+<body>
+
+</body>
+
+</html>
