67a8aa805243a52d15ec55d19a8c197c0e25794d
AlbuquerqueJR <joaquimalbuquerque.ti@gmail.com>
2018-07-24 21:31:09 -0400

    Sessao com ID com PHP

--- session/exemplo3.php
+++ session/exemplo3.php
@@ -1,16 +1,18 @@
 <?php
 
-    //ativando a variavel de sessao
-    session_start();
-    $_SESSION['nome'];
 
+    require_once("config.php");
+
+    echo session_id();
+
+    
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-	<title>Config PHP   </title>
+	<title> SESSION  ID </title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="generator" content="Geany 1.27" />
 </head>
--- session/exemplo4.php
+++ session/exemplo4.php
@@ -1,16 +1,23 @@
 <?php
 
-    //ativando a variavel de sessao
-    session_start();
-    $_SESSION['nome'];
 
+    //session_id('');
+
+    require_once("config.php");
+
+    //gerando o novo ID
+    session_regenerate_id();
+    echo session_id();
+    echo "<br>";
+
+    Var_dump($_SESSION);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-	<title>Config PHP   </title>
+	<title> SESSION   </title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="generator" content="Geany 1.27" />
 </head>
