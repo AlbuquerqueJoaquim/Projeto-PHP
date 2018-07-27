87d09b6cceea5f6beb19d697d3e7ca5d7941c083
AlbuquerqueJR <joaquimalbuquerque.ti@gmail.com>
2018-07-26 22:49:33 -0400

    Function em PHP - variável por referência

---
+++ function/exemplo5.php
@@ -0,0 +1,33 @@
+<?php
+
+    //passando o valor por referencia &
+
+    $pessoa = array(
+      'nome' => 'Paulo',
+      'idade' => 20
+    );
+    foreach ($pessoa as $value) {
+
+      if (gettype($value) === 'integer') $value += 10;
+
+        echo $value.'<br>';
+    }
+
+    print_r($pessoa);
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title> Function  </title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.27" />
+</head>
+
+<body>
+
+</body>
+
+</html>
