7ae1bdc349972ce48b596468fd9fa9bc762b4984
AlbuquerqueJR <joaquimalbuquerque.ti@gmail.com>
2018-07-26 22:21:44 -0400

    Function em PHP

---
+++ function/exemplo2.php
@@ -0,0 +1,30 @@
+<?php
+
+
+    Function ola($texto = "Mundo", $periodo="Bom dia"){
+
+      return "Olá $texto! $periodo! <br>";
+
+    }
+
+    echo ola("Mundo", "Bom dia");
+    echo ola("", "Bom Noite");
+    echo ola("Jose", "Bom Tarde");
+    echo ola("Maria", "");
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title> Function  </title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.27" />
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ function/exemplo3.php
@@ -0,0 +1,27 @@
+<?php
+
+
+    function ola(){
+      $argumentos = func_get_args();
+      return $argumentos;
+
+    }
+
+    var_dump(ola("Bom dia"));
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title> Function  </title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.27" />
+</head>
+
+<body>
+
+</body>
+
+</html>
