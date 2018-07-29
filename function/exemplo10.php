9ef011a4198aedad8f6e91d50613f63b793d00d0
AlbuquerqueJR <joaquimalbuquerque.ti@gmail.com>
2018-07-28 22:30:29 -0400

    Function em PHP

---
+++ function/exemplo10.php
@@ -0,0 +1,27 @@
+<?php
+
+    //colocando a função dentro de uma variavel
+    $fn = function($a){
+
+      var_dump($a);
+
+    };
+
+    $fn("Bobo Chera Chera");
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title> Funcion </title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.27" />
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ function/exemplo9.php
@@ -0,0 +1,30 @@
+<?php
+
+    function test($callback){
+      // lentidão na execução
+      $callback();
+
+    }
+
+    test(function(){
+
+    echo "Terminou!";
+
+  });
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title> Funcion </title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.27" />
+</head>
+
+<body>
+
+</body>
+
+</html>
