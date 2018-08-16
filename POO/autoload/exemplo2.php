609b5093f9771648c64cccea9c05aa2fb209cee2
AlbuquerqueJR <joaquimalbuquerque.ti@gmail.com>
2018-08-15 21:45:49 -0400

    Autoload em PHP carregando varios arquivos.

---
+++ POO/autoload/exemplo2.php
@@ -0,0 +1,37 @@
+<?php
+
+    function incluirClasses($nomeClasse)
+    {
+
+      if(file_exists($nomeClasse.".php") === true)
+      {
+          require_once($nomeClasse.".php");
+      }
+
+    }
+    spl_autoload_register("incluirClasses");
+    spl_autoload_register(function($nomeClasse)
+    {
+        if(file_exists("Abstratas" . DIRECTORY_SEPARATOR . $nomeClasse. ".php") === true){
+          require_once($nomeClasse. ".php");
+        }
+    });
+
+    $carro = new Opala();
+    $carro->acelerar(89);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title> Autoload - PHP  </title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.27" />
+</head>
+
+<body>
+
+</body>
+
+</html>
