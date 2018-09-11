5856b6dac631479c950ae916ac213fc133e989fd
AlbuquerqueJR <joaquimalbuquerque.ti@gmail.com>
2018-09-10 22:25:04 -0400

    Varios exemplos de como listar dados com PHP.

--- DAO/class/usuario.php
+++ DAO/class/usuario.php
@@ -52,6 +52,38 @@ class usuario
       $this->setDtcadastro(new DateTime($row['dtcadastro']));
     }
   }
+#carregando um usuaruo
+  public function getList(){
+    $sql = new Sql();
+    return $sql->SELECT("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+  }
+  #carregando usuario pelo login
+  public static function search($login){
+
+    $sql = new Sql();
+    return $sql->SELECT("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+      ':SEARCH'=>"%".$login."%"
+    ));
+  }
+  #carregando usuario pelo login e SessionHandler
+  public function login($login, $password){
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN and dessenha = :PASSWORD", array(
+      ":LOGIN"=>$login,
+      ":PASSWORD"=>$password
+    ));
+    if (count($results) > 0){
+      $row = $results[0];
+
+      $this->setIdusuario($row['idusuario']);
+      $this->setDeslogin($row['deslogin']);
+      $this->setDessenha($row['dessenha']);
+      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+    } else{
+      throw new \Exception("Error Login ou senha invalidos.");
+
+    }
+  }
   # quais os campos irei mostrar
   public function __toString(){
 
--- DAO/index.php
+++ DAO/index.php
@@ -4,7 +4,19 @@ require_once("config.php");
 #$usuario = $sql->select("SELECT * FROM tb_usuarios");
 #echo json_encode($usuario);
 
-  $user = new usuario();
-  $user->loadById(2);
-  echo $user;
+#  $user = new usuario();
+#  $user->loadById(2);
+#  echo $user;
+#carregar uma lista de tb_usuarios
+//$lista = usuario::getList();
+//echo json_encode($lista);
+
+#Carregar uma lista de usuaruios buscando pelo $login
+//$search = usuario::search("pedro");
+//echo json_encode($search);
+
+#carrega um usuario, usando login e senha
+$usuario = new usuario();
+$usuario->login("root","!@#$%");
+echo $usuario;
  ?>
